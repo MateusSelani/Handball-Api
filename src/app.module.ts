8375201da3bbef0a8d2cdab92820341c5878ea5a
Mateus Selani <selaneitor@gmail.com>
2022-09-20 21:56:06 -0300

    Created entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TeamModule } from './team/team.module';
+import { StadiumModule } from './stadium/stadium.module';
+import { AdressModule } from './adress/adress.module';
+import { MatchModule } from './match/match.module';
+import { TableModule } from './table/table.module';
+import { ChampionshipModule } from './championship/championship.module';
+import { PlayerModule } from './player/player.module';
 
 @Module({
-  imports: [],
+  imports: [PlayerModule, TeamModule, StadiumModule, AdressModule, MatchModule, TableModule, ChampionshipModule],
   controllers: [AppController],
   providers: [AppService],
 })
