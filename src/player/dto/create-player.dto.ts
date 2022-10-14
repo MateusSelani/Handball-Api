e72e07639f4a724847eec732b0833ae5904cfad6
Mateus Selani <selaneitor@gmail.com>
2022-10-14 13:31:21 -0300

    Add validation pipe, transformer and validation in player dto

--- src/player/dto/create-player.dto.ts
+++ src/player/dto/create-player.dto.ts
@@ -1,6 +1,16 @@
+import { IsBoolean, IsNotEmpty, IsString } from "class-validator";
+
 export class CreatePlayerDto {
 
+    @IsString()
+    @IsNotEmpty()
     namePlayer: string;
+    
+    @IsString()
+    @IsNotEmpty()
     genderPlayer: string;
+
+    @IsBoolean()
+    @IsNotEmpty()
     isActivePlayer: boolean;
 }
