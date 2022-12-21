d4fe748ec6e1901981334eee0978725f52b1b515
Mateus Selani <selaneitor@gmail.com>
2022-12-21 12:41:26 -0300

    Correction address service and repository

--- src/adress/adress.service.ts
+++ src/adress/adress.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateAdressDto } from './dto/create-adress.dto';
 import { UpdateAdressDto } from './dto/update-adress.dto';
 import { AdressRepository } from './repositories/adress.repository';
@@ -8,11 +8,19 @@ export class AdressService {
   constructor(private readonly ar: AdressRepository) {}
 
   async create(dto: CreateAdressDto) {
-    await this.ar.create(dto);
-    return dto;
+    if (dto) {
+      await this.ar.create(dto);
+      return dto;
+    } else {
+      throw new NotFoundException(`Invalid adress!`);
+    }
   }
 
   async update(id: string, dto: UpdateAdressDto) {
-    return await this.ar.update(id, dto);
+    if (id && dto) {
+      return await this.ar.update(id, dto);
+    } else {
+      throw new NotFoundException(`Adress not update!`);
+    }
   }
 }
--- src/adress/repositories/adress.repository.ts
+++ src/adress/repositories/adress.repository.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaService } from './../../prisma/prisma.service';
 import { CreateAdressDto } from './../dto/create-adress.dto';
 import { UpdateAdressDto } from './../dto/update-adress.dto';
@@ -8,25 +8,17 @@ export class AdressRepository {
   constructor(private readonly prisma: PrismaService) {}
 
   async create(dto: CreateAdressDto) {
-    if (dto) {
-      return await this.prisma.adress.create({
-        data: dto,
-      });
-    } else {
-      throw new NotFoundException(`Invalid adress!`);
-    }
+    return await this.prisma.adress.create({
+      data: dto,
+    });
   }
 
   async update(id: string, dto: UpdateAdressDto) {
-    if (id && dto) {
-      return await this.prisma.adress.update({
-        where: {
-          idAdress: id,
-        },
-        data: dto,
-      });
-    } else {
-      throw new NotFoundException(`Adress not update!`);
-    }
+    return await this.prisma.adress.update({
+      where: {
+        idAdress: id,
+      },
+      data: dto,
+    });
   }
 }
